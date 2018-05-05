d18c64ee7d56235c149d8e2cf7c783c5733242ec
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-05 23:45:36 +0200

    Create src/commands/fun/GreetingCommand.js

---
+++ src/commands/fun/GreetingCommand.js
@@ -0,0 +1,23 @@
+import { Command } from 'discord.js-commando';
+import { random } from '../../utils/helpers.js';
+import greetings from '../../resources/greetings.js';
+
+class GreetingCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'greeting',
+      aliases: ['hola', 'ola', 'saluda'],
+      group: 'fun',
+      memberName: 'greeting',
+      description: 'Saluda cariñosamente.',
+      examples: ['hola', 'ola']
+    });
+    this.greetings = greetings;
+  }
+
+  run(message) {
+    return message.say(random(this.greetings));
+  }
+}
+
+export default GreetingCommand;
