46b4f9a7d87af8bf57172e1dffbb6514c6dc8c8c
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-05 20:37:10 +0200

    Create src/Trackpoth.js

---
+++ src/Trackpoth.js
@@ -0,0 +1,20 @@
+import { Client } from 'discord.js-commando';
+import path from 'path';
+
+export default class Trackpoth extends Client {
+  constructor(owner, config) {
+    super({ owner });
+    this.groups = config.groups;
+  }
+
+  init() {
+    this.registry.registerGroups(this.groups);
+    this.registry.registerDefaults();
+    this.registry.registerCommandsIn(path.join(__dirname, 'commands'));
+
+    this.on('ready', () => {
+      console.log(`Logged in as ${this.user.tag}.`);
+      this.user.setActivity('Hello World');
+    });
+  }
+}
