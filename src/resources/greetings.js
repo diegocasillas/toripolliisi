54bc94307f62e4d5bb8513bbbe0de5b2db1e371a
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-05 23:44:25 +0200

    Create src/resources/greetings.js

---
+++ src/resources/greetings.js
@@ -0,0 +1,11 @@
+const greetings = [
+  'Hola, feliz Navidad ehehehehehehehe :mrs_claus:',
+  '¡Buenos días! Ah, y en caso de que no os vea luego: ¡buenos días, buenas tardes y buenas noches!',
+  '¡LLEGO EL BIGOTEEEEEEEEEEEES!',
+  'He vuelto del desguace :robot:.',
+  '¡Hola! Me alegro de verte. ¿Qué tal estás hoy? No respondas. No me importa.',
+  'Que la fuerza te acompañe.',
+  'Saludos a todas las niñas guapas. Menos a Reti.'
+];
+
+export default greetings;
