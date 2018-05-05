c381be12a961beaf2abbb8b51f730a3a7f58e1f1
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-05 20:46:50 +0200

    Rename src/config.json to src/config.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Trackpoth from './Trackpoth.js';
 import { owner, token } from '../.env.json';
-import config from './config.json';
+import config from './config.js';
 
 const trackpoth = new Trackpoth(owner, config);
 
