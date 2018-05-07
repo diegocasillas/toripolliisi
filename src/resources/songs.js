ac35bf971584e90508aebce4c377b55207f430b4
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-07 10:46:38 +0200

    Create src/resources/songs.js

---
+++ src/resources/songs.js
@@ -0,0 +1,20 @@
+const songs = [
+  'https://www.youtube.com/watch?v=rY-FJvRqK0E',
+  'https://www.youtube.com/watch?v=6E5m_XtCX3c',
+  'https://www.youtube.com/watch?v=HguKyrS8oWM',
+  'https://www.youtube.com/watch?v=SGUeKCA5p1A',
+  'https://www.youtube.com/watch?v=5ZYgIrqELFw',
+  'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  'https://www.youtube.com/watch?v=RJEblcSr32s',
+  'https://www.youtube.com/watch?v=yF00xX-p28Y',
+  'https://www.youtube.com/watch?v=6wDSFQFKSo0',
+  'https://www.youtube.com/watch?v=1u4xAB_-J74',
+  'https://www.youtube.com/watch?v=odH6449XmKA',
+  'https://www.youtube.com/watch?v=U-ZNfo_NlIU',
+  'https://www.youtube.com/watch?v=qr73XbBFDA8',
+  'https://www.youtube.com/watch?v=oBiVN5T7dv8',
+  'https://www.youtube.com/watch?v=doxUxFsXCJs',
+  'https://www.youtube.com/watch?v=WJRoRt155mA'
+];
+
+export default songs;
