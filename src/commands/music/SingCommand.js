e12b045db39cb9ae7bc1768df5e791ce50f7d064
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-08 20:02:46 +0200

    Add sing forever feature

--- src/commands/music/SingCommand.js
+++ src/commands/music/SingCommand.js
@@ -1,7 +1,7 @@
 import { Command } from 'discord.js-commando';
 import MusicManager from '../../utils/MusicManager.js';
-import { random } from '../../utils/helpers.js';
 import songs from '../../resources/songs.js';
+import { random } from '../../utils/helpers.js';
 
 class SingCommand extends Command {
   constructor(client) {
@@ -10,15 +10,45 @@ class SingCommand extends Command {
       aliases: ['canta', 'cantame'],
       group: 'music',
       memberName: 'sing',
-      description: 'Reproduce una canción aleatoria.'
+      description: 'Reproduce una canción aleatoria.',
+      args: [
+        {
+          key: 'forever',
+          prompt: '¿Reproducir hasta el infinito y más allá?',
+          type: 'string',
+          default: '',
+          validate: (forever) => {
+            const aliases = [
+              'forever', 'siempre', 'mucho', 'infinito'
+            ];
+
+            if (!aliases.includes(forever)) {
+              return '¿Qué? No te entiendo. ¿Querías decir forever?';
+            }
+
+            return true;
+          }
+        }
+      ],
     });
     this.musicManager = new MusicManager();
     this.songs = songs;
   }
 
-  run(message) {
-    const video = random(this.songs);
-    return this.musicManager.join(message, video);
+  run(message, { forever }) {
+    if (!forever) {
+      this.musicManager.play(message, random(this.songs)).catch((error) => {
+        message.reply(error);
+      });
+
+      return;
+    }
+
+    this.musicManager.play(message, random(this.songs)).then((dispatcher) => {
+      this.musicManager.next(dispatcher, message.member.voiceChannel.connection, random(this.songs));
+    }).catch((error) => message.reply(error));
+
+    return;
   }
 }
 
