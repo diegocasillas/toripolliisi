042d9f1a7d585eaa7d661c6e208ca524800f8840
Diego Casillas <diegocasillasdev@gmail.com>
2018-09-04 14:36:00 +0300

    Update greetings.js

--- src/resources/greetings.js
+++ src/resources/greetings.js
@@ -1,7 +1,8 @@
 const greetings = [
-  'Hei! Hyvää Joulua :mrs_claus:',
+  'Moi! Hyvää Joulua :mrs_claus:',
   'Finally someone notices me! I\'ve been here alone for ages!',
-  'May The Force be with you.'
+  'May The Force be with you.',
+  'Hei! You know where to find me if you are in trouble. I can\'t move anyways.'
 ];
 
 export default greetings;
