b8e004932c5a465c9add4d8ac095be9131d8f074
diegocasillas <diegocasillasdev@gmail.com>
2018-10-02 10:41:13 +0300

    Format src/utils/youtube.js file

--- src/utils/youtube.js
+++ src/utils/youtube.js
@@ -1,7 +1,8 @@
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube(process.env.YOUTUBE);
 
-export const searchVideo = name => youtube.searchVideos(name, 1)
-  .then(results => results[0]);
+export const searchVideo = (name) => youtube.searchVideos(name, 1)
+  .then((results) => results[0]);
 
-export const getVideo = url => youtube.getVideo(url).then(result => result);
+export const getVideo = (url) => youtube.getVideo(url)
+  .then((result) => result);
