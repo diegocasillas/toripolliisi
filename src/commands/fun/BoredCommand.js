6b0b43f42787410aed7ce4d98949f2a278c515e3
diegocasillas <diegocasillasdev@gmail.com>
2018-10-02 17:30:52 +0300

    Add bored command

---
+++ src/commands/fun/BoredCommand.js
@@ -0,0 +1,21 @@
+import { Command } from 'discord.js-commando';
+import axios from 'axios';
+
+class BoredCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'bored',
+      group: 'fun',
+      memberName: 'bored',
+      description: 'Suggests you something to do.'
+    });
+  }
+
+  run(message) {
+    axios.get('http://boredapi.com/api/activity/').then((response) => {
+      message.say(response.data.activity);
+    })
+  }
+}
+
+export default BoredCommand;
