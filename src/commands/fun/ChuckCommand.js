8cde7140741e45f9ed197e0332b3808868fb7135
diegocasillas <diegocasillasdev@gmail.com>
2018-10-02 17:40:44 +0300

    Add chuck command

---
+++ src/commands/fun/ChuckCommand.js
@@ -0,0 +1,21 @@
+import { Command } from 'discord.js-commando';
+import axios from 'axios';
+
+class ChuckCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'chuck',
+      group: 'fun',
+      memberName: 'chuck',
+      description: 'Says something about Chuck Norris.'
+    });
+  }
+
+  run(message) {
+    axios.get('https://api.chucknorris.io/jokes/random').then((response) => {
+      message.say(response.data.value, {tts: true});
+    })
+  }
+}
+
+export default ChuckCommand;
